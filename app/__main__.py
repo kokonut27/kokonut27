d1f638cdbda28d6cc06da878cf36fe5e1aff0c23
kokomi simp :3 <beol0127@gmail.com>
2022-01-18 23:08:10 +0000

    chore: setup blog

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
@@ -6,5 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return render_template("index.html")
+
+@app.route('/blog')
+def blog():
+  if request.method == "POST":
+    return
+  if request.method == "GET":
+    return render_template("blog.html")
 
 app.run(host="0.0.0.0", port=8080, debug=True)
