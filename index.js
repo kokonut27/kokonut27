56db2a6a9081611fcc60bb989d0d5616aa338fe3
kokomi simp :3 <beol0127@gmail.com>
2022-04-12 11:58:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Create NPM card 
+const express = require("express");
+const app = express();
+const path = require('path');
+const multer = require('multer');
+const upload = multer();
+const cookieParse = require('cookie-parser');
+
+app.use(cookieParse());
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+app.use(upload.array());
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.enable('verbose errors');
+
+let data = {
+  "username": "kokonut27",
+  "description": "Full-stack developer who loves anime <3",
+  "languages": [
+    "nodejs",
+    "html/css",
+    "python"
+  ]
+};
